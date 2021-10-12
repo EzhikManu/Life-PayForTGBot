86c8e9b6787265db9accc1fbd1a8c4d20ab445ec
EzhikManu <iljasstan@mail.ru>
2021-10-12 19:52:08 +0300

    added steps, allure and selenoid capabilities. Ready for Jenkins

--- src/test/java/com/iljasstan/Life_PayRUTests.java
+++ src/test/java/com/iljasstan/Life_PayRUTests.java
@@ -2,22 +2,42 @@ package com.iljasstan;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.iljasstan.Enums.CashRegistrs;
 import com.iljasstan.Enums.TestDataEnum;
+import config.CredentialsConfig;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selectors.byPartialLinkText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Life_PayRUTests {
+    CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+    String login = credentials.login();
+    String password = credentials.password();
     @BeforeEach
     void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
+        Configuration.remote = String.format("https://%s:%s@selenoid.autotests.cloud/wd/hub/", login, password);
     }
     void accessCookies() {
         if ($("#ok-write-cookie").isDisplayed()) {
@@ -25,35 +45,66 @@ public class Life_PayRUTests {
         else return;
     }
     @Test
+    @DisplayName("Check the registration page with invalids tel and email")
     void registrationTest() {
-        open("https://my.life-pay.ru/?_ga=2.70394262.708319020.1634027069-1543519573.1634027069");
+        step("Open students account page", () -> {
+                    open("https://my.life-pay.ru/?_ga=2.70394262.708319020.1634027069-1543519573.1634027069");
+                });
+        step("open registration page", () -> {
         $("#triggerRegister").click();
-        $("#phoneNum").click();
-        $("#phoneNum").setValue("1111111010");
-        $("#pass").setValue("gavipe2934@settags.com").pressEnter();
-
-        $(".flash-0").shouldHave(Condition.text("Telephone number or email is invalid"));
+        });
+        step("set number and email", () -> {
+                    $("#phoneNum").click();
+                    $("#phoneNum").setValue("1111111010");
+                    $("#pass").setValue("gavipe2934@settags.com").pressEnter();
+                });
+        step("check the message about invalid values", () -> {
+            $(".flash-0").shouldHave(Condition.text("Telephone number or email is invalid"));
+        });
     }
     @EnumSource(value = TestDataEnum.class)
     @ParameterizedTest(name = "Check the values of the top lines")
     void topLineTest(TestDataEnum value) {
-        open("https://life-pay.ru");
-            accessCookies();
-        $(".lf-header_topLineBottomNav").shouldHave(Condition.text(value.getDesc()));
+        step("open main page", () -> {
+                    open("https://life-pay.ru");
+                    accessCookies();
+                });
+        step("check the titles", () -> {
+            $(".lf-header_topLineBottomNav").shouldHave(Condition.text(value.getDesc()));
+        });
     }
     @Test
+    @DisplayName("Check the consultation page is appeared")
     void getConsultationTest() {
-        open("https://life-pay.ru");
-        accessCookies();
-        $(byPartialLinkText("Получить консультацию")).click();
-        assertTrue($("#request_form").isDisplayed());
+        step("open main page", () -> {
+                    open("https://life-pay.ru");
+                    accessCookies();
+                });
+        step("click on the button 'получить консультацию'", () -> {
+                    $(byPartialLinkText("Получить консультацию")).click();
+                });
+        step("check is consultation page appeared", () -> {
+            assertTrue($("#request_form").isDisplayed());
+        });
     }
     @EnumSource(value = CashRegistrs.class)
     @ParameterizedTest(name = "Check the list of cash registrs")
     void listOfCashRegistrsTest(CashRegistrs cashRegistr) {
-        open("https://life-pay.ru");
-        accessCookies();
-        $(".lf-infoBlock2Column").$(byPartialLinkText("Подробнее")).click();
-        $("#item1").shouldHave(Condition.text(cashRegistr.getDesc()));
+        step("open main page", () -> {
+                    open("https://life-pay.ru");
+                    accessCookies();
+                });
+        step("click the button 'Подробнее' at the cash registers tab", () -> {
+                    $(".lf-infoBlock2Column").$(byPartialLinkText("Подробнее")).click();
+                });
+        step("check is consultation page appeared", () -> {
+            $("#item1").shouldHave(Condition.text(cashRegistr.getDesc()));
+        });
+    }
+    @AfterEach
+    void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
