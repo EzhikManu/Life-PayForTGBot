c76b58b7c7285e27ae242aa88b2a36cc714070b5
EzhikManu <iljasstan@mail.ru>
2021-10-13 14:57:01 +0300

    reformated code and optimized imports

--- src/test/java/com/iljasstan/Life_PayRUTests.java
+++ src/test/java/com/iljasstan/Life_PayRUTests.java
@@ -27,6 +27,7 @@ public class Life_PayRUTests {
     CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
     String login = credentials.login();
     String password = credentials.password();
+
     @BeforeEach
     void beforeEach() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
@@ -39,68 +40,74 @@ public class Life_PayRUTests {
         Configuration.startMaximized = true;
         Configuration.remote = String.format("https://%s:%s@selenoid.autotests.cloud/wd/hub/", login, password);
     }
+
     void accessCookies() {
         if ($("#ok-write-cookie").isDisplayed()) {
-        $("#ok-write-cookie").click();}
-        else return;
+            $("#ok-write-cookie").click();
+        } else return;
     }
+
     @Test
     @DisplayName("Check the registration page with invalid tel and email")
     void registrationTest() {
         step("Open students account page", () -> {
-                    open("https://my.life-pay.ru/?_ga=2.70394262.708319020.1634027069-1543519573.1634027069");
-                });
+            open("https://my.life-pay.ru/?_ga=2.70394262.708319020.1634027069-1543519573.1634027069");
+        });
         step("open registration page", () -> {
-        $("#triggerRegister").click();
+            $("#triggerRegister").click();
         });
         step("set number and email", () -> {
-                    $("#phoneNum").click();
-                    $("#phoneNum").setValue("1111111010");
-                    $("#pass").setValue("gavipe2934@settags.com").pressEnter();
-                });
+            $("#phoneNum").click();
+            $("#phoneNum").setValue("1111111010");
+            $("#pass").setValue("gavipe2934@settags.com").pressEnter();
+        });
         step("check the message about invalid values", () -> {
             $(".flash-0").shouldHave(Condition.text("Telephone number or email is invalid"));
         });
     }
+
     @EnumSource(value = TestDataEnum.class)
     @ParameterizedTest(name = "Check the values of the top lines")
     void topLineTest(TestDataEnum value) {
         step("open main page", () -> {
-                    open("https://life-pay.ru");
-                    accessCookies();
-                });
+            open("https://life-pay.ru");
+            accessCookies();
+        });
         step("check the titles", () -> {
             $(".lf-header_topLineBottomNav").shouldHave(Condition.text(value.getDesc()));
         });
     }
+
     @Test
     @DisplayName("Check the consultation page is appeared")
     void getConsultationTest() {
         step("open main page", () -> {
-                    open("https://life-pay.ru");
-                    accessCookies();
-                });
+            open("https://life-pay.ru");
+            accessCookies();
+        });
         step("click on the button 'получить консультацию'", () -> {
-                    $(byPartialLinkText("Получить консультацию")).click();
-                });
+            $(byPartialLinkText("Получить консультацию")).click();
+        });
         step("check is consultation page appeared", () -> {
             assertTrue($("#request_form").isDisplayed());
         });
     }
+
     @EnumSource(value = CashRegistrs.class)
     @ParameterizedTest(name = "Check the list of cash registrs")
     void listOfCashRegistrsTest(CashRegistrs cashRegistr) {
         step("open main page", () -> {
-                    open("https://life-pay.ru");
-                    accessCookies();
-                });
+            open("https://life-pay.ru");
+            accessCookies();
+        });
         step("click the button 'Подробнее' at the cash registers tab", () -> {
-                    $(".lf-infoBlock2Column").$(byPartialLinkText("Подробнее")).click();
-                });
+            $(".lf-infoBlock2Column").$(byPartialLinkText("Подробнее")).click();
+        });
         step("check is consultation page appeared", () -> {
             $("#item1").shouldHave(Condition.text(cashRegistr.getDesc()));
         });
     }
+
     @AfterEach
     void tearDown() {
         Attach.screenshotAs("Last screenshot");
